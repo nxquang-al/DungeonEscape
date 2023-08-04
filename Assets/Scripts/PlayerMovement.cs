a9cbaf69a9add8619e1c99f8c07c0b7fea301ca8
ngxquang <nq23092016@gmail.com>
2023-08-04 15:35:54 +0700

    feat: add Player ladder and shoot logics

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,10 +9,22 @@ public class PlayerMovement : MonoBehaviour
     private SpriteRenderer sprite;
     private Animator anim;
 
+    [SerializeField] private LayerMask jumpableGround;
+
     private float dirX = 0f;
-    private bool isShooting = false;
+    private float dirY = 0f;
+    public bool isShooting = false;
+    private bool isLadder = false;
+    private bool isClimbing = false;
+    private bool facingLeft = false;
+    private int remainBullet = 10;
+
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 14f;
+    [SerializeField] private float climbSpeed = 7f;
+
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject bulletPrefab;
 
     private enum MovementState {idle, running, jumping, falling, ladder, shooting};
     private MovementState state = MovementState.idle;
@@ -24,20 +36,47 @@ public class PlayerMovement : MonoBehaviour
         coll = GetComponent<BoxCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+
+        if (facingLeft){
+            Flip();
+        }
     }
 
     // Update is called once per frame
     private void Update()
     {
         dirX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+        dirY = Input.GetAxisRaw("Vertical");
+        
+        if(!isClimbing || IsGrounded()){
+            rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+        }
 
-        if(Input.GetButtonDown("Jump")){
+        if(Input.GetButtonDown("Jump") && IsGrounded()){
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        if(Input.GetButtonDown("Fire1")  && IsGrounded() && remainBullet > 0){
+            isShooting = true;
+        }
+
+        if(Input.GetButtonDown("Vertical") && isLadder && Mathf.Abs(dirY) > 0f){
+            isClimbing = true;
         }
 
         UpdateAnimationState();
 
+    }
+
+    private void FixedUpdate()
+    {
+        if(isClimbing){
+            rb.gravityScale = 0f;
+            rb.velocity = new Vector2(rb.velocity.x, dirY * climbSpeed);
+        }
+        else{
+            rb.gravityScale = 3f;
+        }
     }
 
     private void UpdateAnimationState()
@@ -46,11 +85,17 @@ public class PlayerMovement : MonoBehaviour
 
         if(dirX > 0f){
             state = MovementState.running;
-            sprite.flipX = false;
+            // sprite.flipX = false;
+            if (facingLeft == true){
+                Flip();
+            }
         }
         else if(dirX < 0f){
             state = MovementState.running;
-            sprite.flipX = true;
+            // sprite.flipX = true;
+            if (facingLeft == false){
+                Flip();
+            }
         }
         else{
             state = MovementState.idle;
@@ -63,11 +108,46 @@ public class PlayerMovement : MonoBehaviour
             state = MovementState.falling;
         }
 
+        if(isShooting){
+            state = MovementState.shooting;
+            isShooting = false;
+            Shoot();
+            remainBullet -= 1;
+        }
+
+        if(isLadder && isClimbing){
+            state = MovementState.ladder;
+        }
+
         anim.SetInteger("state", (int)state); 
     }
 
     private bool IsGrounded()
     {
-        return true;
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.CompareTag("Ladder")){
+            isLadder = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision){
+        if(collision.CompareTag("Ladder")){
+            isLadder = false;
+            isClimbing = false;
+        }
+    }
+
+    private void Flip()
+    {
+        facingLeft = !facingLeft;
+        transform.Rotate(0f, 180f, 0f);
+    }
+
+    private void Shoot()
+    {   
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
     }
 }
