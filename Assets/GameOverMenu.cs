672910eda703eef129e3830f8359f3ac5a54bd66
Nguyễn Quang Tân <81596255+nguyenquangtan@users.noreply.github.com>
2023-08-14 10:25:57 +0700

    fix middle scenes

---
+++ Assets/GameOverMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+}
